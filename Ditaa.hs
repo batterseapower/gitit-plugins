a16d95444eccf799f424d8546de43fe385474ff4
Max Bolingbroke <batterseapower@hotmail.com>
2010-04-03 13:47:21 +0100

    Better diagnostics for not being able to find JAR

--- Ditaa.hs
+++ Ditaa.hs
@@ -16,7 +16,7 @@ module Ditaa (plugin) where
 -- ~~~
 --
 -- The "java" executable must be in the path, and "ditaaXXX.jar" must be in the
--- current directory.
+-- current directory (which will typically be your wiki's root directory).
 --
 -- The generated png file will be saved in the static img directory, using a unique
 -- name will be generated from a hash of the contents.
@@ -65,7 +65,7 @@ renderDitaa outfile contents no_separation no_shadows = unlessM (doesFileExist o
         hClose temp_file_h
   
         -- 2) Run ditaa to turn into an equivalently named .png:
-        ditaa_jar <- findDitaaJar
+        ditaa_jar <- getCurrentDirectory >>= findDitaaJar
         let options = ["-jar", ditaa_jar,
                        "-e", "utf8", -- UTF8 input (I think!)
                        "-o"          -- Overwrite existing file if present (shouldn't be necessary)
@@ -78,11 +78,10 @@ renderDitaa outfile contents no_separation no_shadows = unlessM (doesFileExist o
            then copyFile (replaceExtension temp_file ".png") outfile
            else error $ "Error running ditaa: " ++ stderr_out
 
--- | Find the ditaaXXX.jar file in the current directory
-findDitaaJar :: IO FilePath
-findDitaaJar = fmap (fromMaybe (error "Could not locate the ditaa .jar file in the current directory") . maybeHead .
-                     filter ("ditaa" `isInfixOf`) . filter (".jar" `isSuffixOf`)) $
-                    getCurrentDirectory >>= getDirectoryContents
+-- | Find the ditaaXXX.jar file in the given directory
+findDitaaJar :: FilePath -> IO FilePath
+findDitaaJar dir = fmap (fromMaybe (error $ "Could not locate the ditaa .jar file in the directory " ++ dir) . maybeHead .
+                         filter ("ditaa" `isInfixOf`) . filter (".jar" `isSuffixOf`)) $ getDirectoryContents dir
 
 withTempFile :: FilePath -- ^ Temporary directory to create the file in
              -> String   -- ^ File name template: see 'openTempFile'
